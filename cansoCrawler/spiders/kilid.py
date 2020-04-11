008bd943ea0a301d364a97848144552089fe988c
MostafaGhanbari <godhelot1@outlook.com>
2020-04-12 00:06:14 +0430

    fix kilid

--- cansoCrawler/spiders/kilid.py
+++ cansoCrawler/spiders/kilid.py
@@ -6,192 +6,158 @@ import scrapy
 
 class KilidSpider(scrapy.Spider):
     name = 'kilid'
-    allowed_domains = ['kilid.com', 'kilidstatic.com']
+    allowed_domains = ['kilid.com']
     start_urls = ['https://api.kilid.com/api/location/getAllCities/portal']
-    type = "buy"
+    deal_type = "buy"
     page_count = 3
+
+    def __init__(self, deal_type='buy', **kwargs):
+        if deal_type == "rent":
+            self.deal_type = "rent"
+        else:
+            self.deal_type = "buy"
+        super().__init__(**kwargs)
 
     def parse(self, response):
         city_dict = json.loads(response.body.decode("UTF-8"))
         city_list = city_dict["content"]
         for city in city_list:
-            yield response.follow(
-                "https://www.kilidstatic.com/kilid-portal-web/information/p_city_{}.json".format(city["locationId"]),
-                callback=self.parse_city_sector)
+            body = {"locations": [{"type": "city", "locationId": str(city["locationId"])}], "subType": self.deal_type,
+                    "type": "listing",
+                    "sort": "kilid,DESC"}
+            for page in range(1, self.page_count):
+                yield scrapy.Request(
+                    "https://api.kilid.com/api/listing/search/portal/v2.0?page={}&sort=kilid,DESC".format(
+                        page),
+                    method='post',
+                    body=json.dumps(body), headers={"Content-Type": "application/json"},
+                    callback=self.parse_ads,
+                    cb_kwargs={"page": page})
 
-    def parse_city_sector(self, response):
-        city_info_dict = json.loads(response.body.decode("UTF-8"))
-        location = [{"type": "city", "locationId": city_info_dict["city"][0]["locationId"]}]
-        for section_key, section_list in city_info_dict.items():
-            if section_key == "city":
-                continue
-            for section in section_list:
-                if len(location) > 1:
-                    location.pop()
-                location.append({"type": section_key, "locationId": section["locationId"]})
-                for category_key, category_value in self.home_type_dict.items():
-                    last_sub_category = "sub"
-                    body = {"locations": list(location), "subType": self.type, "type": "listing",
-                            "sort": "kilid,DESC", "utilityType": category_key, "sub": True}
-                    for sub_category in category_value["child"]:
-                        body[sub_category["nameLat"]] = body.pop(last_sub_category)
-                        last_sub_category = sub_category["nameLat"]
-                        # for page_number in range(1, self.page_count):
-                        yield scrapy.Request(
-                            "https://api.kilid.com/api/listing/search/portal/v2.0?page={}&sort=kilid,DESC".format(
-                                1),
-                            method='post',
-                            body=json.dumps(body), headers={"Content-Type": "application/json"},
-                            callback=self.parse_ads,
-                            cb_kwargs={"city": city_info_dict["city"][0]["nameFa"],
-                                       "neigh": section["nameFa"],
-                                       "category": category_value["name"],
-                                       "sub_category": sub_category["nameFa"], "page": 1})
-
-    def parse_ads(self, response, city, neigh, category, sub_category, page):
+    def parse_ads(self, response, page):
         ads_list = json.loads(response.body.decode("UTF-8"))["content"]
         if len(ads_list) == 0:
             return
         for ad in ads_list:
-            if "title" not in ad:
-                continue
-            yield {
-                "page": page,
-                "subject": ad["title"],
-                "city": city,
-                "neigh": neigh,
-                "category": category,
-                "sub_category": sub_category
-            }
+            if "id" in ad.keys():
+                yield response.follow(url="https://api.kilid.com/api/listing/{}/single/v5".format(ad["id"]),
+                                      callback=self.parse_ad, cb_kwargs={"page": page})
+
+    def parse_ad(self, response, page):
+        ad = json.loads(response.body.decode('UTF-8'))
+        yield {
+            "id": ad["listingId"],
+            "subject": ad["title"],
+            "page": page,
+            "rent": ad["rent"],
+            "deposit": ad["deposit"],
+            "price": ad["price"],
+            "deal_type": self.deal_type
+        }
 
     home_type_dict = {
-        "residential": {
+        1: {
             "name": "مسکونی",
-            "id": 1,
-            "child": [
-                {
-                    "id": 1,
+            "child": {
+                1: {
                     "nameLat": "apartment",
                     "nameFa": "آپارتمان"
                 },
-                {
-                    "id": 2,
+                2: {
                     "nameLat": "villa",
                     "nameFa": "ویلایی"
                 },
-                {
-                    "id": 3,
+                3: {
                     "nameLat": "land",
                     "nameFa": "زمین/کلنگی"
                 },
-                {
-                    "id": 18,
+                18: {
                     "nameLat": "pentHouse",
                     "nameFa": "پنت هاوس"
                 },
-                {
-                    "id": 11,
+                11: {
                     "nameLat": "tower",
                     "nameFa": "برج"
                 },
-                {
-                    "id": 12,
+                12: {
                     "nameLat": "swuite",
                     "nameFa": "سوییت"
                 },
-                {
-                    "id": 13,
+                13: {
                     "nameLat": "realEstate",
                     "nameFa": "مستغلات"
                 }
-            ]
+            }
         },
-        "commercial": {
+        2: {
             "name": "تجاری",
-            "id": 2,
-            "child": [
-                {
-                    "id": 5,
+            "child": {
+                5: {
                     "nameLat": "store",
                     "nameFa": "مغازه"
                 },
-                {
-                    "id": 3,
+                3: {
                     "nameLat": "land",
                     "nameFa": "زمین/کلنگی"
                 },
-                {
-                    "id": 13,
+                13: {
                     "nameLat": "realEstate",
                     "nameFa": "مستغلات"
                 },
-                {
-                    "id": 4,
+                4: {
                     "nameLat": "garden",
                     "nameFa": "باغ/باغچه"
                 },
-                {
-                    "id": 2,
+                2: {
                     "nameLat": "villa",
                     "nameFa": "ویلایی"
                 }
-            ]
+            }
         },
-        "office": {
+        3: {
             "name": "اداری",
-            "id": 3,
-            "child": [
-                {
-                    "id": 1,
+            "child": {
+                1: {
                     "nameLat": "apartment",
                     "nameFa": "آپارتمان"
                 },
-                {
-                    "id": 3,
+                3: {
                     "nameLat": "land",
                     "nameFa": "زمین/کلنگی"
                 },
-                {
-                    "id": 13,
+                13: {
                     "nameLat": "realEstate",
                     "nameFa": "مستغلات"
                 }
-            ]
+            }
         },
-        "industrial": {
+        4: {
             "name": "صنعتی",
-            "id": 4,
-            "child": [
-                {
-                    "id": 15,
+            "child": {
+                15: {
                     "nameLat": "factory",
                     "nameFa": "کارخانه"
                 },
-                {
-                    "id": 17,
+                17: {
                     "nameLat": "workshop",
                     "nameFa": "کارگاه"
                 },
-                {
-                    "id": 16,
+                16: {
                     "nameLat": "prpStorage",
                     "nameFa": "انبار/سوله"
                 },
-                {
-                    "id": 13,
+                13: {
                     "nameLat": "realEstate",
                     "nameFa": "مستغلات"
                 },
-                {
-                    "id": 3,
+                3: {
                     "nameLat": "land",
                     "nameFa": "زمین/کلنگی"
                 },
-                {
-                    "id": 4,
+                4: {
                     "nameLat": "garden",
                     "nameFa": "باغ/باغچه"
                 }
-            ]
+            }
         },
     }
