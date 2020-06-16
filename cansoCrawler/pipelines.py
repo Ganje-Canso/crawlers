6c2ae3d3b7f3ec5935ba675c30059c6dce7b36bb
MostafaGhanbari <godhelot1@outlook.com>
2020-06-16 15:06:09 +0430

    fix nullable fields in car

--- cansoCrawler/pipelines.py
+++ cansoCrawler/pipelines.py
@@ -39,8 +39,70 @@ class CansocrawlerPipeline(object):
     def save_car_data(self, item):
         try:
             self.cursor.execute(
-                f"call save_car_data({item['token']}, {item['source_id']}, {item['time']}, '{item['title']}', '{item['category']}', '{item['sub_category']}', '{item['province']}', '{item['city']}', '{item['neighbourhood']}', {item['production']}, {item['price']}, '{item['description']}', '{item['url']}', '{item['thumbnail']}', {item['latitude']}, {item['longitude']}, '{item['tell']}', {item['swap']}, '{item['brand']}', {item['consumption']}, '{item['color']}', '{item['cash_installment']}', '{item['gear_box']}', '{item['company']}', '{item['chassis_type']}', '{item['model']}', '{item['body_condition']}', '{item['fuel']}')")
+                "insert into car (" +
+                (("token," if self.insertThis(item["token"]) else "") +
+                 ("source_id," if self.insertThis(item["source_id"]) else "") +
+                 ("time," if self.insertThis(item["time"]) else "") +
+                 ("title," if self.insertThis(item["title"]) else "") +
+                 ("category," if self.insertThis(item["category"]) else "") +
+                 ("sub_category," if self.insertThis(item["sub_category"]) else "") +
+                 ("province," if self.insertThis(item["province"]) else "") +
+                 ("city," if self.insertThis(item["city"]) else "") +
+                 ("neighbourhood," if self.insertThis(item["neighbourhood"]) else "") +
+                 ("production," if self.insertThis(item["production"]) else "") +
+                 ("price," if self.insertThis(item["price"]) else "") +
+                 ("description," if self.insertThis(item["description"]) else "") +
+                 ("url," if self.insertThis(item["url"]) else "") +
+                 ("thumbnail," if self.insertThis(item["thumbnail"]) else "") +
+                 ("latitude," if self.insertThis(item["latitude"]) else "") +
+                 ("longitude," if self.insertThis(item["longitude"]) else "") +
+                 ("tell," if self.insertThis(item["tell"]) else "") +
+                 ("swap," if self.insertThis(item["swap"]) else "") +
+                 ("brand," if self.insertThis(item["brand"]) else "") +
+                 ("consumption," if self.insertThis(item["consumption"]) else "") +
+                 ("color," if self.insertThis(item["color"]) else "") +
+                 ("cash_installment," if self.insertThis(item["cash_installment"]) else "") +
+                 ("gear_box," if self.insertThis(item["gear_box"]) else "") +
+                 ("company," if self.insertThis(item["company"]) else "") +
+                 ("chassis_type," if self.insertThis(item["chassis_type"]) else "") +
+                 ("model," if self.insertThis(item["model"]) else "") +
+                 ("body_condition," if self.insertThis(item["body_condition"]) else "")).strip(',') +
+                (",fuel" if self.insertThis(item["fuel"]) else "") +
+                ") " +
+                "values (" +
+                ((f"{item['token']}," if self.insertThis(item["token"]) else "") +
+                 (f"{item['source_id']}," if self.insertThis(item["source_id"]) else "") +
+                 (f"{item['time']}," if self.insertThis(item["time"]) else "") +
+                 (f"'{item['title']}'," if self.insertThis(item["title"]) else "") +
+                 (f"'{item['category']}'," if self.insertThis(item["category"]) else "") +
+                 (f"'{item['sub_category']}'," if self.insertThis(item["sub_category"]) else "") +
+                 (f"'{item['province']}'," if self.insertThis(item["province"]) else "") +
+                 (f"'{item['city']}'," if self.insertThis(item["city"]) else "") +
+                 (f"'{item['neighbourhood']}'," if self.insertThis(item["neighbourhood"]) else "") +
+                 (f"{item['production']}," if self.insertThis(item["production"]) else "") +
+                 (f"{item['price']}," if self.insertThis(item["price"]) else "") +
+                 (f"'{item['description']}'," if self.insertThis(item["description"]) else "") +
+                 (f"'{item['url']}'," if self.insertThis(item["url"]) else "") +
+                 (f"'{item['thumbnail']}'," if self.insertThis(item["thumbnail"]) else "") +
+                 (f"{item['latitude']}," if self.insertThis(item["latitude"]) else "") +
+                 (f"{item['longitude']}," if self.insertThis(item["longitude"]) else "") +
+                 (f"'{item['tell']}'," if self.insertThis(item["tell"]) else "") +
+                 (f"{item['swap']}," if self.insertThis(item["swap"]) else "") +
+                 (f"'{item['brand']}'," if self.insertThis(item["brand"]) else "") +
+                 (f"{item['consumption']}," if self.insertThis(item["consumption"]) else "") +
+                 (f"'{item['color']}'," if self.insertThis(item["color"]) else "") +
+                 (f"'{item['cash_installment']}'," if self.insertThis(item["cash_installment"]) else "") +
+                 (f"'{item['gear_box']}'," if self.insertThis(item["gear_box"]) else "") +
+                 (f"'{item['company']}'," if self.insertThis(item["company"]) else "") +
+                 (f"'{item['chassis_type']}'," if self.insertThis(item["chassis_type"]) else "") +
+                 (f"'{item['model']}'," if self.insertThis(item["model"]) else "") +
+                 (f"'{item['body_condition']}'," if self.insertThis(item["body_condition"]) else "")).strip(',') +
+                (f",'{item['fuel']}'" if self.insertThis(item["fuel"]) else "") +
+                ")")
             self.conn.commit()
         except:
             self.conn.rollback()
         return item
+
+    def insertThis(self, v):
+        return str(v) != '-1' and v != 'not_defined'
